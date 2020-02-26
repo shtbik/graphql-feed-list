e24ea8ca581396d1e190e656a2fe609d7b089be3
Aleksandr Shtykov <aashtykov@dasreda.ru>
2020-02-26 15:44:59 +0300

    static build 0.1.0

---
+++ dist/precache-manifest.1db7b953a19a8083755d1509c6e241f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e754addb769b98bbe25",
+    "url": "/static/css/main.57610c9f.chunk.css"
+  },
+  {
+    "revision": "3e754addb769b98bbe25",
+    "url": "/static/js/main.3e754add.chunk.js"
+  },
+  {
+    "revision": "39dfefd9de5afd7a29c1",
+    "url": "/static/js/1.39dfefd9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "843145e79fbec36e34d34ffa057b7c59",
+    "url": "/index.html"
+  }
+];
